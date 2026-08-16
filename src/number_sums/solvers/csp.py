06b2fba114fe500a78697f48e49de8086a9bf3e3
rei-luisinho <filipe.medeiros@apps.pm.pb.gov.br>
2026-08-15 22:54:00 -0300

    New solver CSP class with constructor and properties

--- src/number_sums/solvers/csp.py
+++ src/number_sums/solvers/csp.py
@@ -74,3 +74,104 @@ class _MutableStats:
 
     def freeze(self) -> CSPStats:
         return CSPStats(self.nodes, self.backtracks, self.propagations)
+
+
+class NumberSumsCSPSolver:
+    """
+    Solves Number Sums using constraint propagation and backtracking.
+
+    Args:
+        board          : Square matrix of positive integers.
+        row_targets    : Target for each row.
+        column_targets : Target for each column.
+
+    Use :meth:`from_game` to construct the solver from a game instance. The
+    solving methods do not modify the game or retain state between searches.
+    """
+
+    REMOVE : DomainValue = 0
+    KEEP   : DomainValue = 1
+
+    MAX_SIZE = 8
+
+    def __init__(
+        self,
+        board          : Sequence[Sequence[int]],
+        row_targets    : Sequence[int],
+        column_targets : Sequence[int],
+        *,
+        game: NumberSumsGame | None = None,
+    ) -> None:
+        self._board          = tuple(tuple(row) for row in board)
+        self._row_targets    = tuple(row_targets   )
+        self._column_targets = tuple(column_targets)
+
+        self._validate_problem()
+
+        self._game = game
+
+        self._variables   = tuple(
+            (row, column)
+            for row    in range(self.size)
+            for column in range(self.size)
+        )
+        self._constraints = self._build_constraints()
+
+        constraints_by_variable: dict[Coordinate, list[int]] = {
+            variable : [] for variable in self._variables
+        }
+        for index, constraint in enumerate(self._constraints):
+            for variable in constraint.variables:
+                constraints_by_variable[variable].append(index)
+
+        self._constraints_by_variable = {
+            variable : tuple(indices)
+            for variable, indices in constraints_by_variable.items()
+        }
+
+        self._neighbors = {
+            variable : frozenset(
+                neighbor
+                for index    in self._constraints_by_variable[variable]
+                for neighbor in self._constraints[index].variables
+                if  neighbor != variable
+            )
+            for variable in self._variables
+        }
+
+    @classmethod
+    def from_game(cls, game: NumberSumsGame) -> NumberSumsCSPSolver:
+        """Creates a solver linked to the game so that hints use its current state"""
+
+        return cls(
+            game.board         ,
+            game.row_targets   ,
+            game.column_targets,
+
+            game=game,
+        )
+
+    @property
+    def size(self) -> int:
+        return len(self._board)
+
+    @property
+    def variables(self) -> tuple[Coordinate, ...]:
+        return self._variables
+
+    @property
+    def constraints(self) -> tuple[SumConstraint, ...]:
+        return self._constraints
+
+    @property
+    def neighbors(self) -> Mapping[Coordinate, frozenset[Coordinate]]:
+        return self._neighbors.copy()
+
+    @property
+    def domains(self) -> Mapping[Coordinate, frozenset[DomainValue]]:
+        """Initial CSP domains, exposed as immutable copies"""
+
+        return {
+            variable : frozenset((self.REMOVE, self.KEEP))
+            for variable in self._variables
+        }
