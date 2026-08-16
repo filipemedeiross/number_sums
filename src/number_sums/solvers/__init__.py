05cae811aecf6f677de1a08bfc98ba87fa4eb7e7
rei-luisinho <filipe.medeiros@apps.pm.pb.gov.br>
2026-08-15 22:30:19 -0300

    New submodule solvers

---
+++ src/number_sums/solvers/__init__.py
@@ -0,0 +1,18 @@
+from ..utils import Hint
+
+from .csp    import CSPResult          , \
+                    CSPStep            , \
+                    CSPStats           , \
+                    NumberSumsCSPSolver, \
+                    SumConstraint
+
+
+__all__ = [
+    "Hint",
+
+    "CSPResult"          ,
+    "CSPStep"            ,
+    "CSPStats"           ,
+    "NumberSumsCSPSolver",
+    "SumConstraint"      ,
+]
